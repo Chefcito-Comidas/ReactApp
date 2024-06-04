35cdc943db900870672af362ce5608c98cb310cf
Santiago39353535 <santiagomarinaro1@gmail.com>
2024-06-04 19:38:40 -0300

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
-import { useState } from 'react';
 import * as formik from 'formik';
 import * as yup from 'yup';
 
